cc958d6ba6dc5247f02791b502041ebcf4e7824d
sbcreations14378 <dsak@gmail.com>
2023-09-09 22:06:40 +0530

    Added CRUD operations in API

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,5 +18,53 @@ router.get('/:id',(req,res)=>{
     }
 });
 
+router.post('/',(req,res)=>{
+    const newUser={
+        id:uuid.v4(),
+        name:req.body.name,
+        email:req.body.email
+    }
+if(!newUser.name || !newUser.email){
+    return res.sendStatus(400);
+}
+else{
+    users.push(newUser);
+    res.json(users)
+}
 
+});
+
+
+router.put('/:id',(req,res)=>{
+    const found= users.some(user=>user.id === parseInt(req.params.id));
+    if (found){
+        const updateUser=req.body;
+        users.forEach((user)=>{
+            if(user.id === parseInt(req.params.id)){
+                user.name= updateUser.name? updateUser.name:user.name;
+                user.email= updateUser.email? updateUser.email:user.email;
+                res.json({msg:"user Updated",user});
+
+            }
+        });
+    }
+    else{
+        // res.write("Request Unable to Process! Because Requested user not found..");
+        res.sendStatus(400);
+    }
+});
+
+router.delete('/:id',(req,res)=>{
+    const found=users.some(user=>user.id === parseInt(req.params.id));
+    if (found){
+        users = users.filter(user =>user.id !== parseInt(req.params.id));
+        res.json({
+            msg:"User Deleted!",
+            users
+        });
+    }
+    else{
+        res.sendStatus(400);
+    }
+});
 module.exports = router
