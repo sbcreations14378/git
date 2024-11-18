477c19e37560d70ec56595093ad676036fd82761
dsak789 <dsak.official@gmail.com>
2024-11-18 20:12:24 +0530

    Vercel Deploy

--- ExpressJS/src/index.js
+++ ExpressJS/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
@@ -16,6 +17,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+app.use(cors())
 app.use(express.json());
 app.use(loggingMiddleWare);
 app.use(cookieParser("practice"));
